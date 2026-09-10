532c6b5442de2ebeca5b8c59efdbfd8059055466
Muhammad Hafiz <hafiz@axcell.my>
2026-09-10 23:26:21 +0800

    fix: sign release APK so it can be installed on devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
